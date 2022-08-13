47b96579186d499317df46b333220c6599ea94a2
Rameez <muhammadrameezahmed73@gmail.com>
2022-08-13 17:32:00 +0500

    Element  Page component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import './App.css';
-
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Error from "./Error";
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<div>Home page</div>}/>
-      <Route path="/about" element={<div> About</div>}/>
-      <Route path="/contact" element={<div>contact us </div>}/>
-      <Route path="*" element={<div>Error</div>} />
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="*" element={<Error/>} />
     </Routes>
 
 
